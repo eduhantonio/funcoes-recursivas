2711c7c9feefd8ef80fbee6cf8d40c55968cecdc
Eduardo Antonio <126982066+eduhantonio@users.noreply.github.com>
2024-04-08 16:36:35 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -22,7 +22,15 @@ public class Main
 	    // Imprimir somente os múltiplos de 3
 	    int[] arrayTres = {3, 6, 9, 10, 11, 12};
 	    divisores(arrayTres, arrayTres.length - 1);
+
+	    // Fazer a somatória de 1 até "N"
+        	System.out.println(somador(7));
+        
+       	 // Somar somente os pares de 1 até "N"
+	    System.out.println(somaPares(7));
 	    
+	    // Somar somente os impares de 1 até "N"
+	    System.out.println(somaImpares(7));
 	}
     
     // Soma de valores em um Array
@@ -67,4 +75,24 @@ public class Main
                 System.out.print("["+multiplos[indice]+"]");
         }
     }
+
+	// Fazer a somatória de 1 até "N"
+    public static int somador(int contaSoma){
+        if (contaSoma == 1) return 1;
+        return contaSoma + somador(contaSoma - 1);
+    }
+        
+    // Somar somente os pares de 1 até "N"
+	public static int somaPares(int contaPar){
+	    if (contaPar == 1) return 0;
+	    if (contaPar % 2 == 0) return contaPar + somaPares(contaPar - 1);
+	    else return somaPares(contaPar - 1);
+	}
+	    
+	// Somar somente os impares de 1 até "N"
+	public static int somaImpares(int contaImpar){
+	    if (contaImpar == 1) return 1;
+	    if (contaImpar % 2 == 1) return contaImpar + somaImpares(contaImpar - 1);
+	    else return somaImpares(contaImpar - 1);
+	}
 }
